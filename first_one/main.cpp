14226d66059fb97211327a1be65cd9d4878768c3
CrazyAlan <xiaochuan.chen91@gmail.com>
2015-02-06 20:21:44 -0800

    Initial Commit

---
+++ first_one/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  first_one
+//
+//  Created by CA on 2015-02-06.
+//  Copyright (c) 2015 CA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
