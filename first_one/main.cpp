e62b35e26656fa9dea931c1336d0cd872632e814
CrazyAlan <xiaochuan.chen91@gmail.com>
2015-02-13 21:47:01 -0800

    Successed Panoroma with 2 images

--- first_one/main.cpp
+++ first_one/main.cpp
@@ -37,11 +37,14 @@ Mat norm_matrix = Mat(3, 3, CV_32FC1,norm_ma);
 
 /// Function header
 void siftDetector( int, void* );
+void goodMatches(Mat descriptor1, Mat descriptor2,std::vector<DMatch>* good_matches);
 Mat buildA(std::vector<KeyPoint> keypoint_1, std::vector<KeyPoint> keypoint_2, std::vector<DMatch> good_matches);
-Mat buildCoord(void);
+Mat buildCoord(int x_min, int x_max, int y_min, int y_max);
 Mat coordTransX2Xprime(Mat x,Mat H); // H X2Xprime H.inv() Xprime2X
 Mat coordCalib(Mat x,bool flag);
 Mat getTrans(std::vector<KeyPoint> keypoint_1, std::vector<KeyPoint> keypoint_2, std::vector<DMatch> good_matches);
+void affineTrans(Mat* s_image, Mat* out_img, Mat coord_source, Mat coord_out);
+Mat linearBlend(Mat img1, Mat img2);
 
 /**
  * @function main
@@ -79,68 +82,79 @@ void siftDetector( int, void* )
     detector.operator()(src_gray_1, Mat(), keypoints_1,descriptor_1,false);
     detector.operator()(src_gray_2,Mat(),keypoints_2,descriptor_2,false);
     
-    //Feature Matching
-    FlannBasedMatcher matcher;
-    std::vector<DMatch> matches;
-    matcher.match(descriptor_1, descriptor_2, matches);
-    double max_dist = 0; double min_dist = 100;
-    
-    //Caculate max and min distances between keypoints
-    for (int i =0 ; i < descriptor_1.rows; i++)
-    {
-        double dist = matches[i].distance;
-        if( dist < min_dist ) min_dist = dist;
-        if( dist > max_dist ) max_dist = dist;
-    }
-    
-    //Draw Good Matches
-    std::vector< DMatch > good_matches;
-    
-    for( int i = 0; i < descriptor_1.rows; i++ )
-    { if( matches[i].distance <= max(3*min_dist, 0.02) )
-    { good_matches.push_back( matches[i]); }
-    }
-    
+    vector<DMatch> good_matches;
+    goodMatches(descriptor_1, descriptor_2,&good_matches);
     //Mat img_matches;
-    //drawMatches(src_1, keypoints_1, src_2, keypoints_2, good_matches, img_matches);
+
     Mat H =  getTrans(keypoints_1, keypoints_2, good_matches);
-    cout << "H is " << H << endl;
+   // cout << "H is " << H << endl;
     
-    Mat coord = buildCoord();
+    float tmp[4][3] = {{0,0,1},{1600,0,1},{0,1200,1},{1600,1200,1}};
+    Mat m_tmp(4,3,CV_32FC1,tmp);
+    Mat board_coord = m_tmp.t();
     
-    Mat H_X = coordTransX2Xprime(coord, H); // Refered Coordinate
-    H_X = coordCalib(H_X,TO_EXTENDED_COORD); //Extended Coordinate
+    Mat H_board_coord = coordTransX2Xprime(board_coord, H); // Refered Coordinate
+    H_board_coord = coordCalib(H_board_coord,TO_EXTENDED_COORD); //Extended Coordinate
+   // cout << "H_board_coord" << H_board_coord << endl;
+    
+    double x_min,x_max,y_min,y_max;
+    
+    minMaxIdx(H_board_coord.row(0), &x_min, &x_max);
+    minMaxIdx(H_board_coord.row(1), &y_min, &y_max);
+    cout << "x min" << endl << x_min << "  " <<  x_max  << "  "<< y_min << "  " << y_max << endl;
+
+    
+    Mat out_img_coord = buildCoord(x_min, x_max, y_min,y_max);
+    Mat img_coord = coordCalib(out_img_coord, TO_REFERED_COORD);
+    
+    cout << "out_img_coor "<< endl << out_img_coord(Range(0, 3),Range(0, 20)) << endl;
+    img_coord = coordTransX2Xprime(img_coord, H.inv());
     
     Mat out_img1((int)pano_rows,(int)pano_cols,CV_8UC1,Scalar::all(0));
     Mat out_img2((int)pano_rows,(int)pano_cols,CV_8UC1,Scalar::all(0));
+
+    affineTrans(&src_gray_1, &out_img1, img_coord, out_img_coord);
     src_gray_2.copyTo(out_img2(Range((int)offset_rows,(int)rows+(int)offset_rows),Range((int)offset_cols,(int)offset_cols+(int)cols)));
     
     
-    
-    //cout << "Src Gray"<< endl<< src_gray_2.at<float>(1197, 1452)<< endl;
-    for (int i=0; i<(int)rows; i++) {
-        for (int j=0; j<(int)cols; j++) {
-            int x = H_X.at<float>(0,i*(int)cols + j);
-            int y = H_X.at<float>(1,i*(int)cols + j);
-           // cout << src_gray_1.at<uchar>(i, j) << endl;
-            out_img1.at<uchar>(y,x) = src_gray_1.at<uchar>(i, j);
-        }
-    }
-    
-    double alpha = 0.5;
-    double beta = 1 - alpha;
-    Mat dst;
-    addWeighted(out_img1, alpha, out_img2, beta, 1, dst);
+    Mat dst = linearBlend(out_img1, out_img2);
     
     
-    cout << "Coord 1 " << endl << H_X(Range(0,3),Range(0, 10)) << endl;
+    
     resize(dst , dst, Size(dst.cols/4,out_img1.rows/4));
     namedWindow(matches_window,0);
     imshow( "Good Matches", dst);
     
 }
 
-/** 
+
+/**
+ * @function goodMatches
+ * @find Mathces
+ */
+void goodMatches(Mat descriptor1, Mat descriptor2,std::vector<DMatch> *good_matches)
+{
+    //Feature Matching
+    FlannBasedMatcher matcher;
+    std::vector<DMatch> matches;
+    matcher.match(descriptor1, descriptor2, matches);
+    double max_dist = 0; double min_dist = 100;
+
+    //Caculate max and min distances between keypoints
+    for (int i =0 ; i < descriptor1.rows; i++)
+    {
+        double dist = matches[i].distance;
+        if( dist < min_dist ) min_dist = dist;
+            if( dist > max_dist ) max_dist = dist;
+                }
+
+    //Draw Good Matches
+    for( int i = 0; i < descriptor1.rows; i++ )
+    { if( matches[i].distance <= max(3*min_dist, 0.02) )
+    { (*good_matches).push_back( matches[i]); }
+    }
+}
+/**
  * @function buildA
  * @build Matrix A for DLT algorithm
  */
@@ -188,14 +202,16 @@ Mat getTrans(std::vector<KeyPoint> keypoint_1, std::vector<KeyPoint> keypoint_2,
     return H;
 }
 
-Mat buildCoord(void)
+Mat buildCoord(int x_min, int x_max, int y_min, int y_max)
 {
-    Mat coord = Mat(3, (int)cols*(int)rows, CV_32FC1);
-    for (int i=0; i<(int)rows; i++) {
-        for (int j=0; j<(int)cols; j++) {
-            coord.at<float>(0,i*(int)cols + j) = j;
-            coord.at<float>(1,i*(int)cols + j) = i;
-            coord.at<float>(2,i*(int)cols + j) = 1;
+    int x_range = x_max - x_min;
+    int y_range = y_max - y_min;
+    Mat coord = Mat(3, x_range*y_range, CV_32FC1);
+    for (int i=0; i<(int)y_range; i++) {
+        for (int j=0; j<(int)x_range; j++) {
+            coord.at<float>(0,i*x_range + j) = j + x_min;
+            coord.at<float>(1,i*x_range + j) = i + y_min;
+            coord.at<float>(2,i*x_range + j) = 1;
         }
     }
     return coord;
@@ -203,26 +219,60 @@ Mat buildCoord(void)
 
 Mat coordTransX2Xprime(Mat x, Mat H)
 {
-   // Mat x_prime(3,1,CV_32FC1);
-    x = norm_matrix*x;//x_norm
-    x = H*x;//h_x
-    x = norm_matrix.inv()*x;
-    for (int i=0; i<x.cols; i++) {
-        x.col(i) /= x.at<float>(2,i);
+    Mat x_prime(3,x.cols,CV_32FC1);
+    x_prime = norm_matrix*x;//x_norm
+    x_prime = H*x_prime;//h_x
+    x_prime = norm_matrix.inv()*x_prime;
+    for (int i=0; i<x_prime.cols; i++) {
+        x_prime.col(i) /= x_prime.at<float>(2,i);
     }
-    return x;
+    return x_prime;
 }
 
 Mat coordCalib(Mat x, bool flag)
 {
+    Mat x_new;
+    x.copyTo(x_new);
     if (flag) {
-        x.row(0) += offset_cols;
-        x.row(1) += offset_rows;
+        x_new.row(0) += offset_cols;
+        x_new.row(1) += offset_rows;
     }else
     {
-        x.row(0) -= offset_cols;
-        x.row(1) -= offset_rows;
+        x_new.row(0) -= offset_cols;
+        x_new.row(1) -= offset_rows;
     }
-        return x;
+        return x_new;
 }
 
+void affineTrans(Mat* s_image, Mat* out_img, Mat coord_source, Mat coord_out)
+{
+    for (int i=0; i<coord_out.cols; i++) {
+        int x = coord_out.at<float>(0,i);
+        int y = coord_out.at<float>(1,i);
+        int x2 = coord_source.at<float>(0,i);
+        int y2 = coord_source.at<float>(1,i);
+        if ( x2 >=0 && x2<1600 && y2 >=0 && y2<1200 ) {
+            (*out_img).at<uchar>(y,x) = (*s_image).at<uchar>(y2,x2);
+        }
+    }
+}
+
+Mat linearBlend(Mat img1, Mat img2)
+{
+    Mat mask(img1.size(),CV_8UC1);
+    Mat mask2(img1.size(),CV_8UC1);
+    cv::min(img1, img2, mask);
+    mask2 = (mask != 0);
+    mask2 /= 255;
+    mask = (mask == 0);
+    mask /= 255;
+    mask *= 2;
+    mask = mask + mask2;
+    Mat out_img(img1.size(),CV_8UC1);
+    double alpha = 0.5;
+    double beta = 1 - alpha;
+    addWeighted(img1, alpha, img2, beta, 0, out_img);
+    out_img = out_img.mul(mask);
+    
+    return out_img;
+}
